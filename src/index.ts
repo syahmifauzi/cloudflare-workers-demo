7bc394341564d0e5f4d09e36b2c398b5f0e8d951
Syahmi Fauzi <brosyahmifauzi@gmail.com>
2024-03-14 01:01:49 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,41 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		/**
+		 * Example someHost is set up to take in a JSON request
+		 * Replace url with the host you wish to send requests to
+		 * @param {string} someHost the host to send the request to
+		 * @param {string} url the URL to send the request to
+		 */
+		const someHost = 'https://jsonplaceholder.typicode.com';
+		const url = someHost + '/todos/2';
+
+		/**
+		 * gatherResponse awaits and returns a response body as a string.
+		 * Use await gatherResponse(..) in an async function to get the response body
+		 * @param {Response} response
+		 */
+		async function gatherResponse(response: Response) {
+			const { headers } = response;
+			const contentType = headers.get('content-type') || '';
+			if (contentType.includes('application/json')) {
+				return JSON.stringify(await response.json());
+			}
+			return response.text();
+		}
+
+		const init = {
+			headers: {
+				'content-type': 'application/json;charset=UTF-8',
+			},
+		};
+
+		const response = await fetch(url, init);
+		const results = await gatherResponse(response);
+		return new Response(results, init);
+	},
+
+	async scheduled(event: Event, env: Env, ctx: ExecutionContext) {
+		console.log('cron processed');
 	},
 };
